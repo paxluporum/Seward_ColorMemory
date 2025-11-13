d4ff5b413a00e5e76995cfbad047e7ff0d5bbf2b
paxluporum <paxluporum@gmail.com>
2025-11-13 11:14:06 -0500

    end of class

--- card.js
+++ card.js
@@ -16,54 +16,62 @@
 
 
 //tutorial below
-let cardList = [
-    "Red",
-    "Blue",
-    "Green",
-    "Yellow",
-    "Orange",
-    "Purple",
-    "Brown",
-]
+// let cardList = [
+//     "Red",
+//     "Blue",
+//     "Green",
+//     "Yellow",
+//     "Orange",
+//     "Purple",
+//     "Brown",
+// ]
 
-let cardSet;
-let board = [];
-let rows = 4;
-let columns = 5;
+// let cardSet;
+// let board = [];
+// let rows = 4;
+// let columns = 5;
 
-window.onload = function() {
-    shuffleCards();
-    startGame();
-}
+// window.onload = function() {
+//     shuffleCards();
+//     startGame();
+// }
 
-function shuffleCards() {
-    cardSet = cardList.concat(cardList); //two of each card
-    console.log(cardSet);
-    //shuffle
-    for (let i = 0; i < cardSet.length; i++) {
-        let j = Math.floor(Math.random() * cardSet.length); //get random index
-        //swap
-        let temp = cardSet[i];
-        cardSet[i] = cardSet[j];
-        cardSet[j] = temp;
-    }
-    console.log(cardSet)
-}
+// function shuffleCards() {
+//     cardSet = cardList.concat(cardList); //two of each card
+//     console.log(cardSet);
+//     //shuffle
+//     for (let i = 0; i < cardSet.length; i++) {
+//         let j = Math.floor(Math.random() * cardSet.length); //get random index
+//         //swap
+//         let temp = cardSet[i];
+//         cardSet[i] = cardSet[j];
+//         cardSet[j] = temp;
+//     }
+//     console.log(cardSet)
+// }
 
-function startGame() {
-    //arange the board 4x5
-    for (let r = 0; r < rows; r++) {
-        for (let c = 0; c < columns; c++) {
-            let cardImg = cardSet.pop();
-            rows.push(cardImg); //JS
+// function startGame() {
+//     //arange the board 4x5
+//     for (let r = 0; r < rows; r++) {
+//         for (let c = 0; c < columns; c++) {
+//             let cardImg = cardSet.pop();
+//             rows.push(cardImg); //JS
 
-            // <img id="0-0" class="card src="red.jpg">
-            let card = document.createElement("img");
-            card.id = r.toString() + "-" + c.toString();
-            card.src = cardImg + ".jpg";
-            card.classList.add("card");
-            document.getElementById("board").append(card);
-        }
-    }
-}
+//             // <img id="0-0" class="card src="red.jpg">
+//             let card = document.createElement("img");
+//             card.id = r.toString() + "-" + c.toString();
+//             card.src = cardImg + ".jpg";
+//             card.classList.add("card");
+//             document.getElementById("board").append(card);
+//         }
+//         board.push(row);
+//     }
+//     console.log(board);
+// }
+
+// function hideCards() {
+//     for (let r = 0; r < rows; r++) {
+//         for
+//     }
+// }
 //tutorial above
--- code.js
+++ code.js
@@ -1,51 +1,64 @@
-import { Toolbox } from "./toolbox.js";
-
-let canvas = document.getElementById("myCanvas");
-let pencil = canvas.getContext("2d"); // This gives you the drawing context, like a pencil
-let toolbox = new Toolbox();
-
-function gameLoop() {
-
-// let myFavoriteLetters = ["a", "b", "c"];
-console.log(toolbox.getRandomColor());
-
-pencil.fillStyle = toolbox.getRandomColor();
-pencil.fillRect(50, 50, 100, 100);
-
-}
-setInterval(gameLoop, 50);
-//HANDLES Timer
-let time = 0;
-
-//score goes up every second
-function raiseTime() {
-    time += 1;
-    let timeElement = document.getElementById("timeDisplay");
-    timeElement.innerHTML = "TIME" + time;
-}
-setInterval(raiseTime, 1000);
-
-
-//Simon's code below
-// import { MemoryButton } from "./memoryButton.js";
 // import { Toolbox } from "./toolbox.js";
 
 // let canvas = document.getElementById("myCanvas");
 // let pencil = canvas.getContext("2d"); // This gives you the drawing context, like a pencil
 // let toolbox = new Toolbox();
 
-// let color1 = toolbox.getRandomColor();
-// let card1a = new MemoryButton(canvas, pencil, 50, 50, color1);
-// let card1b = new MemoryButton(canvas, pencil, 200, 50, color1);
-
-
-
 // function gameLoop() {
 
-//     pencil.clearRect(0,0, canvas.width, canvas.height);
-//     card1a.draw();
-//     card1b.draw();
-// }
+// // let myFavoriteLetters = ["a", "b", "c"];
+// console.log(toolbox.getRandomColor());
 
+// pencil.fillStyle = toolbox.getRandomColor();
+// pencil.fillRect(50, 50, 100, 100);
+
+// }
 // setInterval(gameLoop, 50);
+// //HANDLES Timer
+// let time = 0;
+
+// //score goes up every second
+// function raiseTime() {
+//     time += 1;
+//     let timeElement = document.getElementById("timeDisplay");
+//     timeElement.innerHTML = "TIME" + time;
+// }
+// setInterval(raiseTime, 1000);
+
+
+//Simon's code below
+import { MemoryButton } from "./memoryButton.js";
+import { Toolbox } from "./toolbox.js";
+
+let canvas = document.getElementById("myCanvas");
+let pencil = canvas.getContext("2d"); // This gives you the drawing context, like a pencil
+let toolbox = new Toolbox();
+
+let color1 = toolbox.getRandomColor();
+let card1a = new MemoryButton(canvas, pencil, 50, 50, color1);
+let card1b = new MemoryButton(canvas, pencil, 200, 50, color1);
+
+//my code below
+let color2 = toolbox.getRandomColor();
+let card2a = new MemoryButton(canvas, pencil, 50, 200, color2);
+let card2b = new MemoryButton(canvas, pencil, 200, 200, color2);
+
+let color3 = toolbox.getRandomColor();
+let card3a = new MemoryButton(canvas, pencil, 50, 350, color3);
+let card3b = new MemoryButton(canvas, pencil, 200, 350, color3);
+
+
+
+function gameLoop() {
+
+    pencil.clearRect(0,0, canvas.width, canvas.height);
+    card1a.draw();
+    card1b.draw();
+    card2a.draw();
+    card2b.draw();
+    card3a.draw();
+    card3b.draw();
+}
+
+setInterval(gameLoop, 50);
 //Simon's code above
